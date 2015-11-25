ce1f6cb62701e6609714f0853676180339c1c687
Glen <glen@silverstripe.com>
2015-11-25 16:12:30 +1300

    [minor] changed gallery from many_many to has_many relationship with GameObjects and tidied up code to Silverstripe coding conventions

--- code/HomePage.php
+++ code/HomePage.php
@@ -1,59 +1,36 @@
 <?php
 class HomePage extends Page {
 
-  private static $description = "The Pocketwatch Home Page";
-
-  private static $can_be_root = true;
-
-	private static $db = array(
-	);
+	private static $description = "The Pocketwatch Home Page";
 
 	private static $has_one = array(
-    'HeaderImage' => 'Image'
+		'HeaderImage' => 'Image'
 	);
 
-  function getCMSFields() {
-    $fields = parent::getCMSFields();
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
 
-    $fields->addFieldToTab(
-        'Root.Upload',
-        $uploadField = new UploadField(
-            $name = 'HeaderImage',
-            $title = 'Please upload an image to display in your header.')
-    );
+		$fields->addFieldToTab(
+			'Root.Upload',
+			$uploadField = new UploadField(
+				'HeaderImage',
+				'Please upload an image to display in your header.'
+			)
+		);
 
-    $uploadField->setFolderName('Header');
-    $uploadField->setAllowedFileCategories('image');
+		$uploadField->setFolderName('Header');
+		$uploadField->setAllowedFileCategories('image');
 
-
-    return $fields;
-  }
+		return $fields;
+	}
 
 }
 class HomePage_Controller extends ContentController {
 
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	private static $allowed_actions = array (
-	);
-	public function LatestGallery() {
-		
-		return GalleryObject::get()
-				->sort('SortOrder', 'ASC')
-				->limit(3);
-	}
+	 public function LatestGallery() {
+	 	return GalleryObject::get()
+	 			->sort('SortID', 'ASC')
+	 			->limit(3);
+	 }
 
 }
