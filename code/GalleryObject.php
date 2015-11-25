f86874e6ebd3885d7a0a2a2227e701024db9c643
Glen <glen@silverstripe.com>
2015-11-25 13:25:27 +1300

    [enhancement] modified to have image thumbnail displayed in gallery instead of full image

--- code/GalleryObject.php
+++ code/GalleryObject.php
@@ -4,27 +4,46 @@
 */
 class GalleryObject extends DataObject {
 
-    public static $db=array(
-        'Title' => 'Varchar',
-        'Description'=>'Text'
+  public static $db=array(
+    'Title' => 'Varchar',
+    'Description'=>'Text'
+  );
+
+  private static $has_one = array(
+    'GalleryImage' => 'Image'
+  );
+
+  function getCMSFields() {
+    $fields = parent::getCMSFields();
+
+    $fields->addFieldToTab(
+        'Root.Main',
+        $uploadField = new UploadField(
+            $name = 'GalleryImage',
+            $title = 'Please upload an image to display showcase in your gallery.')
     );
-    private static $has_one = array(
-      'GalleryImage' => 'Image'
-    );
-    function getCMSFields() {
-      $fields = parent::getCMSFields();
 
-      $fields->addFieldToTab(
-          'Root.Main',
-          $uploadField = new UploadField(
-              $name = 'GalleryImage',
-              $title = 'Please upload an image to display showcase in your gallery.')
-      );
+    $uploadField->setFolderName('GalleryImages');
+    $uploadField->setAllowedFileCategories('image');
 
-      $uploadField->setFolderName('GalleryImages');
-      $uploadField->setAllowedFileCategories('image');
+    return $fields;
+  }
 
-      return $fields;
-    }
+  // this function get the cms thumbnail for the summary fields to use
+  public function ImageThumbnail() {
+    return $this->GalleryImage()->CMSThumbnail();
+  }
+
+    // Tell the datagrid what fields to show in the table
+  public static $summary_fields = array(
+    'ImageThumbnail' => 'Thumbnail',
+    'Title' => 'Title',
+    'Description' => 'Type'
+  );
+
+  static $searchable_fields = array(
+    'Title',
+    'Description'
+  );
 
 }
