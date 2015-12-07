10bf8ae80b3078f0ec6fac2af7034af396a4a5ef
Glen Peek <glen@silverstripe.com>
2015-12-08 12:11:35 +1300

    NEW a free text slice that only appears on the home page

---
+++ code/TextSlice.php
@@ -0,0 +1,27 @@
+<?php
+class TextSlice extends Page {
+
+    private static $description = "Pocketwatch: A Plain Text Slice that is displayed on the home page";
+
+    private static $has_one = array(
+        'BackgroundImage' => 'Image',
+    );
+
+    function getCMSFields() {
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldToTab(
+            'Root.Image Upload',
+            $uploadHeroField = new UploadField(
+                'BackgroundImage',
+                'Please upload an image to be used as the Background Image for your Text Slice.'
+            )
+        );
+
+        $uploadHeroField->setFolderName('TextSlice');
+        $uploadHeroField->setAllowedFileCategories('image');
+
+        return $fields;
+    }
+}
+class TextSlice_Controller extends ContentController {}
---
+++ templates/Includes/TextSlice.ss
@@ -0,0 +1,8 @@
+<section class="parallax-1 contact-section" style="background-image: url($BackgroundImage.Url)" >
+  <div class="container">
+    <div class="col-sm-8 col-sm-offset-2 text-center contact-section-text-area">
+        <h2>$Title</h2>
+        $Content
+    </div>
+  </div>
+</section>
