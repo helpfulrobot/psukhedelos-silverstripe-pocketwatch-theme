6146d641681dd5980393ac07521d5476eff90faf
Glen Peek <glen.peek@gmail.com>
2015-12-10 09:05:40 +1300

    API getFeatureByCategoryID and getBlogSlicePosts (not quite working as expected yet)

--- code/extensions/BlogExtension.php
+++ code/extensions/BlogExtension.php
@@ -14,4 +14,51 @@ class BlogExtension extends DataExtension {
                 )->setTitle('Featured Posts')->setName('FeaturedPostsOptions')
             );
     }
+
+    public function getFeaturedByCategoryID($id){
+
+        return DataObject::get(
+            'BlogPost',
+            'FeaturedPost = 1 AND BlogCategoryID = '.$id,
+            'PublishDate DESC'
+        )   ->leftJoin('BlogPost_Categories','BlogPost_Categories.BlogPostID = BlogPost.ID')
+            ->first();
+
+    }
+
+    public function getBlogSlicePosts(){
+
+        $featurePost = DataObject::get(
+            'BlogPost',
+            'FeaturedPost = 1',
+            'PublishDate DESC'
+        )   ->first();
+
+        if($featurePost){
+            $otherPosts = DataObject::get(
+                'BlogPost',
+                'BlogPost.ID != '.$featurePost->ID,
+                'PublishDate DESC'
+            )->limit(2);
+
+//            $otherPosts->limit(3);
+//            $otherPosts->add($featurePost);
+//            array_unshift($otherPosts , $featurePost);
+//            var_dump($otherPosts);
+//            die();
+
+        }else{
+            $otherPosts = DataObject::get(
+                'BlogPost',
+                '',
+                'PublishDate DESC'
+            )->limit(3);
+        }
+
+
+
+//        var_dump($otherPosts);
+//        die();
+        return $otherPosts;
+    }
 }
