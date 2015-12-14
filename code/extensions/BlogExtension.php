af14d7fa65108c6a4c1e5ff44cef721d765c74c7
Glen Peek <glen@silverstripe.com>
2015-12-14 17:20:17 +1300

    API add functionality to get featured posts by Tag and Archive

--- code/extensions/BlogExtension.php
+++ code/extensions/BlogExtension.php
@@ -25,13 +25,52 @@ class BlogExtension extends DataExtension {
         return null;
     }
 
+    public function getTag(){
+        $params = Controller::curr()->getURLParams();
+
+        if (isset($params["Tag"])) {
+            return $params["Tag"];
+        }
+
+        return null;
+    }
+
+    public function getArchive(){
+        $params = Controller::curr()->getURLParams();
+
+        if (isset($params["Year"])) {
+
+            $archive = Array(
+                "Year" => $params["Year"],
+                "Month" => $params["Month"],
+                "Day" => $params ["Day"]
+            );
+
+            return $archive;
+        }
+
+        return null;
+    }
+
     public function getFeaturedBlogPost(){
         $category = $this->getCategory();
+        $tag = $this->getTag();
+        $archive = $this->getArchive();
 
         if ($category) {
             $category = BlogCategory::get()->filter("URLSegment", $category)->first();
 
             return $category->BlogPosts()->filter("FeaturedPost", true)->first();
+        }
+
+        if ($tag) {
+            $tag = BlogTag::get()->filter("URLSegment", $tag)->first();
+
+            return $tag->BlogPosts()->filter("FeaturedPost", true)->first();
+        }
+
+        if ($archive) {
+            return $this->owner->getArchivedBlogPosts($archive["Year"], $archive["Month"], $archive ["Day"])->filter("FeaturedPost", true)->first();
         }
 
         return BlogPost::get()->filter("FeaturedPost", true)->first();
