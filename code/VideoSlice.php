6733a730c130315e00fb2a5faefc5f765f819a97
Luke Ryley <lry6003@gmail.com>
2016-01-22 13:41:56 +1300

    Added Features to VideoSlice
    Added Start Time
    Added Auto Start
    Added Disable Keyboard

--- code/VideoSlice.php
+++ code/VideoSlice.php
@@ -1,43 +1,56 @@
 <?php
 class VideoSlice extends Page {
-  
+
 
     private static $description = "Pocketwatch: A Page that is used to embed Youtube Videos into the homepage";
 
     private static $allowed_children = false;
-    
+
     static $db = array(
         'VideoURL' => 'Text',
         'VideoURL2' => 'Text',
+        'VideoStart' => 'Int',
+        'VideoAutoPlay' => 'Boolean',
+        'DisableKeyboardCtrl' => 'Boolean'
     );
-    
+
     static $defaults = array(
         'ShowInMenus' => false
     );
-    
+
     function getCMSFields() {
         $fields = parent::getCMSFields();
-      
+
         $fields->addFieldToTab(
             'Root.Youtube',
             new TextField('VideoURL', "Youtube Video ID")
         );
-        
+
+        $fields->addFieldToTab(
+            'Root.Youtube',
+            new TextField('VideoStart', "Video Start Time")
+        );
+
         $fields->addFieldToTab(
             'Root.Youtube',
             new CheckboxField('VideoAutoPlay', "Auto Play?")
         );
-        
+
+        $fields->addFieldToTab(
+            'Root.Youtube',
+            new CheckboxField('DisableKeyboardCtrl', "Disable Keyboard Controls")
+        );
+
         $fields->addFieldToTab(
             'Root.Youtube',
             new TextField('VideoURL2', "Youtube Video ID #2")
         );
-        
+
         $fields->addFieldToTab(
             'Root.Youtube',
             new CheckboxField('VideoAutoPlay2', "Auto Play #2?")
         );
-    
+
         return $fields;
     }
 }
